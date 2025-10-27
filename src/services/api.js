c5d59e4256eebbeca507095ffbf837706c70d6b3
joewaweruwanjema <wawerujoe@gmail.com>
2025-10-27 21:02:31 +0300

    Integrate all REST endpoints (auth, farms, reports, weather, etc.)

---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptor to add the token to every request
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
